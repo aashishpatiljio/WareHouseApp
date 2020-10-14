9d50cdc9356c4c12ddf1441f7af7dc72978d073f
aashish <aashish@DESKTOP-DS8DN3D>
2020-10-15 03:28:05 +0530

    03:27 A.M (15-10-2020)

--- src/main/java/in/nareshit/aashish/view/ShipmentTypePdfView.java
+++ src/main/java/in/nareshit/aashish/view/ShipmentTypePdfView.java
@@ -14,6 +14,7 @@ import com.lowagie.text.Document;
 import com.lowagie.text.Element;
 import com.lowagie.text.Font;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
@@ -32,18 +33,27 @@ public class ShipmentTypePdfView extends AbstractPdfView {
 		@SuppressWarnings("unchecked")
 		List<ShipmentType> list = (List<ShipmentType>) model.get("list"); //down-cast
 
-		Font titleFont = new Font(Font.HELVETICA, 22, Font.BOLD, Color.BLUE);	
+		//-- TITLE--
+		Font titleFont = new Font(Font.HELVETICA, 22, Font.BOLD, Color.BLUE);
+		//Paragraph(string,font)
 		Paragraph title = new Paragraph("SHIPMENT TYPES", titleFont); //to set title of doc
 		title.setAlignment(Element.ALIGN_CENTER);
+	
+		//--DATE AND TIME--
 		Paragraph date = new Paragraph(new Date().toString()); //to set doc gen date
 
+		//creating table
 		PdfPTable table = new PdfPTable(6);
-		table.addCell("ID");
-		table.addCell("MODE");
-		table.addCell("CODE");
-		table.addCell("ENABLE SHIPMENT");
-		table.addCell("GRADE");
-		table.addCell("DESCRIPTION");
+		table.setSpacingBefore(15.0f);
+		table.setTotalWidth(new float[] {1.0f,1.5f,1.5f,2.5f,2.0f,3.0f});
+		Font headingFont = new Font(Font.HELVETICA, 15, Font.BOLD, new Color(140,6,251));
+		
+		table.addCell(new Phrase("ID", headingFont));
+		table.addCell(new Phrase("MODE", headingFont));
+		table.addCell(new Phrase("CODE", headingFont));
+		table.addCell(new Phrase("ENABLE SHIPMENT", headingFont));
+		table.addCell(new Phrase("GRADE", headingFont));
+		table.addCell(new Phrase("DESCRIPTION", headingFont));
 
 		for(ShipmentType st:list) {
 			table.addCell(String.valueOf(st.getId()));
@@ -56,8 +66,8 @@ public class ShipmentTypePdfView extends AbstractPdfView {
 
 		//add the elements to the document
 		document.add(title);
-		document.add(date);	
 		document.add(table);
+		document.add(date);	
 
 	}
 
